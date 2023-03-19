73895cc3c3ea6f8f2c4a6685123d9284e9d006ab
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-03-19 16:13:09 +0200

    Added titles to UserCard

--- src/elements/UserCard.tsx
+++ src/elements/UserCard.tsx
@@ -5,10 +5,10 @@ const UserCard: FC<{user: User}> = ({user}) => {
     return (
         <div className={"card"}>
             <div className={"card_photo"}><img src={user.photo} alt="avatar"/></div>
-            <div className={"card_name"}>{user.name}</div>
-            <div className={"card_content"}>{user.position}</div>
-            <div className={"card_content"}>{user.email}</div>
-            <div className={"card_content"}>{user.phone}</div>
+            <div title={user.name} className={"card_name"}>{user.name}</div>
+            <div title={user.position} className={"card_content"}>{user.position}</div>
+            <div title={user.email} className={"card_content"}><a href={`mailto:${user.email}`}>{user.email}</a></div>
+            <div title={user.phone} className={"card_content"}><a href={`tel:${user.phone}`}>{user.phone}</a></div>
         </div>
     );
 };

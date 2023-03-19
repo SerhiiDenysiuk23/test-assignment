288982551407ade4dfd0364bf3123b4c34b59804
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-03-19 19:58:46 +0200

    Added preload

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React, {useState} from 'react';
+import React, {lazy, useState, Suspense} from 'react';
 import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Users from "./components/Users";
 import FormSection from "./components/FormSection";
+
+const Banner = lazy(() => import ("./components/Banner"))
+const Users = lazy(() => import ("./components/Users"))
 
 function App() {
     const [queryPick, setQueryPick] = useState<boolean>(false)
     const handlePick = () => setQueryPick(!queryPick)
 
-  return (
-    <div className="App">
-        <Header/>
-        <Banner/>
-        <Users pick={queryPick}/>
-        <FormSection pickFunc={handlePick}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Suspense fallback={<div className={"preload"}><img src="./preloader.png" alt="loading..."/></div>}>
+                <Banner/>
+                <Users pick={queryPick}/>
+            </Suspense>
+            <FormSection pickFunc={handlePick}/>
+        </div>
+    );
 }
 
 export default App;
